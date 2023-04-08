a9b357a99df8802369bc2f1c3f12a1382179168c
phlpp77 <PhilippHemkemeyer@iCloud.com>
2023-04-08 18:47:23 -0400

    Add scene changes

--- iOS/VibeSensAr/VibeSensAr/ContentView.swift
+++ iOS/VibeSensAr/VibeSensAr/ContentView.swift
@@ -9,30 +9,52 @@ import SwiftUI
 import RealityKit
 
 struct ContentView : View {
+    @State var completion: ((Entity?) -> ()) = { _ in }
+    @State var arView = ARView(frame: .zero)
+    @State var boxScene = try! Experience.loadPortal()
+    @State var feedbackText = "No update"
+    
     var body: some View {
-        ARViewContainer().edgesIgnoringSafeArea(.all)
+        ZStack {
+            ARViewContainer(arView: $arView, boxScene: $boxScene)
+                .ignoresSafeArea()
+            VStack {
+                Button("Stop") {
+                    //                        boxScene.steelBox?.stopAllAudio()              // 1
+                    //                        boxScene.steelBox?.stopAllAnimations()         // 1
+                    print("Actions are stopped.")
+                    //
+                    completion = {
+                        print("completion done")
+                        $0?.stopAllAudio()
+                        feedbackText = "Room opened"
+                    }
+                    //                            $0?.stopAllAudio()                         // 2
+                    //                            $0?.stopAllAnimations()                    // 2
+                    //                            $0?.scale = [1,15,1]
+                    //                            print("Both actions were completely stopped.")
+                    //                        }
+                    //
+                    boxScene.actions.roomOpened.onAction = completion
+                    //                        boxScene.actions.occured.onAction = completion
+                }
+                Text(feedbackText)
+                Spacer()
+            }
+        }
     }
 }
 
 struct ARViewContainer: UIViewRepresentable {
     
-    func makeUIView(context: Context) -> ARView {
-        
-        let arView = ARView(frame: .zero)
-        
-        // Load the room scene from the "Experience" Reality File
-        let portalAnchor = try! Experience.loadPortal()
-//        let roomAnchor = try! Experience.loadRoom()
-        
-        // Add the box anchor to the scene
-        arView.scene.anchors.append(portalAnchor)
-//        arView.scene.anchors.append(roomAnchor)
-        
-        return arView
-        
-    }
+    @Binding var arView: ARView
+    @Binding var boxScene: Experience.Portal
     
-    func updateUIView(_ uiView: ARView, context: Context) {}
+    func makeUIView(context: Context) -> ARView {
+        arView.scene.anchors.append(boxScene)
+        return arView
+    }
+    func updateUIView(_ view: ARView, context: Context) { }
     
 }
 
